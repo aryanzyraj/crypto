6330b42b5c2a124162e0b5eab4af7642894b3e32
Aryan Raj <34100584+aryanzyraj@users.noreply.github.com>
2025-02-15 23:21:12 +0530

    Delete bicrypto_mobile_3.3 directory

--- bicrypto_mobile_3.3/android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package bicrypto.com
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- bicrypto_mobile_3.3/lib/backend/api_requests/interceptors.dart
+++
@@ -1,3 +0,0 @@
-export 'api_interceptor.dart';
-
-export '/custom_code/actions/upload_interceptor.dart' show UploadInterceptor;
